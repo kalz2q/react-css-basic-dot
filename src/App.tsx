3297c71201d0742486d844d558bb87f00dc888b6
kalz2q <katsu95i@gmail.com>
2022-09-16 15:14:39 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,8 @@ function App() {
   return (
     <div className="App">
       <div className="box1">Box 1</div>
-      <div className="box2">Box 2</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
-      <div className="box3">Box 3</div>
+      <div className="box2">
+        <div className="inner-box">Box 2</div></div>
       <div className="box3">Box 3</div>
     </div>
   );
